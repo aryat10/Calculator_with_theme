0c92fe27caacdecb4a5a86fba30403cbf00377ec
Aryat Srivastava <107941072+aryat10@users.noreply.github.com>
2023-05-29 09:54:34 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ buttons.forEach((item) => {    // implementaion of for each loop in JS
   item.onclick = () => {
     if (item.id == "clear") {
       display.innerText = ""; // 
-    } else if (item.id == "backspace") {
+    } else if (item.id == "backspace") { // 
       let string = display.innerText.toString();
       display.innerText = string.substr(0, string.length - 1);   
     } else if (display.innerText != "" && item.id == "equal") {
